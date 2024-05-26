0776ac56dc321fa3d23989e227e1cc2aa62aba3d
EugenRodin <gameover59630@gmail.com>
2024-05-26 18:32:21 +0200

    fixed https.5

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,31 @@
+const { fromEvent, from, of } = rxjs
+const {
+  map, filter, debounceTime, distinctUntilChanged, tap, switchMap, catchError,
+} = rxjs.operators
+
 const moviesListElement = document.querySelector('#movies-list')
 const searchInput = document.querySelector('#search')
 const searchCheckbox = document.querySelector('#checkbox')
 
-let lastSearchQuery = null
-let isSearchTriggerEnabled = false
-
-const debounceTime = (() => {
-  let timer = null
-  return (cb, ms) => {
-    if (timer) {
-      clearTimeout(timer)
-      timer = null
-    }
-    timer = setTimeout(cb, ms)
-  }
-})()
-
-const getData = url => fetch(url)
+const fetchData = url => fetch(url)
   .then(res => res.json())
   .then(data => {
     if (!data || !data.Search) throw new Error('Сервер повернув некоректні дані!')
     return data.Search
   })
 
+const getMovies = searchQuery => from(fetchData(`https://www.omdbapi.com/?apikey=18b8609f&s=${searchQuery}`)).pipe(
+  catchError(err => {
+    console.error('Помилка при завантаженні фільмів:', err.message)
+    return of([])
+  }),
+)
+
 const addMovieToList = movie => {
   const item = document.createElement('div')
   const img = document.createElement('img')
 
   item.classList.add('movie')
-
   img.classList.add('movie__image')
   img.src = /^(http|https):\/\//i.test(movie.Poster) ? movie.Poster : 'img/no-image.png'
   img.alt = `${movie.Title} ${movie.Year}`
@@ -38,24 +35,15 @@ const addMovieToList = movie => {
   moviesListElement.prepend(item)
 }
 
-const clearMoviesMarkup = () => {
-  if (moviesListElement) moviesListElement.innerHTML = ''
-}
+const searchMoviesStream$ = fromEvent(searchInput, 'input')
+  .pipe(
+    map(e => e.target.value.trim()),
+    filter(value => value.length > 3),
+    debounceTime(2000),
+    distinctUntilChanged(),
+    tap(() => !searchCheckbox.checked && (moviesListElement.innerHTML = '')),
+    switchMap(searchQuery => getMovies(searchQuery)),
+    tap(movies => movies.forEach(addMovieToList)),
+  )
 
-const inputSearchHandler = e => {
-  debounceTime(() => {
-    const searchQuery = e.target.value.trim()
-
-    if (!searchQuery || searchQuery.length < 4 || searchQuery === lastSearchQuery) return
-    if (!isSearchTriggerEnabled) clearMoviesMarkup()
-
-    getData(`http://www.omdbapi.com/?apikey=18b8609f&s=${searchQuery}`)
-      .then(movies => movies.forEach(addMovieToList))
-      .catch(err => console.error(err))
-
-    lastSearchQuery = searchQuery
-  }, 1000)
-}
-
-searchInput.addEventListener('input', inputSearchHandler)
-searchCheckbox.addEventListener('change', e => (isSearchTriggerEnabled = e.target.checked))
+searchMoviesStream$.subscribe()
